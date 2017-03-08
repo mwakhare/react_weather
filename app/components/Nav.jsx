c22a962d7306c3c65ef4642b93b6a3e7bcd3ebfb
mwakhare <mwakhare@torinit.com>
2017-03-08 16:36:26 +0530

    basic top-bar ready

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,14 +3,37 @@ var {Link, IndexLink} = require ('react-router');
 
 
 var Nav = React.createClass ({
+  onSearch: function (e){
+    e.preventDefault();
+    alert ("Not yet wired up!")
+  },
   render: function (){
     return (
-      <div>
-      <h3>Nav component</h3>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Get Weather</IndexLink>
-      <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
-      <IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
-      </div>
+        <div className="top-bar">
+          <div class="top-bar-left">
+            <ul className="dropdown menu" data-dropdown-menu>
+              <li className="menu-text">Weather App</li>
+              <li>
+                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Get Weather</IndexLink>
+              </li>
+              <li>
+                <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+              </li>
+              <li>
+                <IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+              </li>
+            </ul>
+          </div>
+          <div className="top-bar-right">
+            <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li><input type="search" placeholder="Search"/></li>
+
+              <li><button type="button" className="button">Search</button></li>
+            </ul>
+            </form>
+          </div>
+        </div>
     );
   }
 });
@@ -19,3 +42,11 @@ module.exports = Nav;
 
 
 //<a href="#/about">Go to About Page</a>
+
+
+// <div>
+// <h3>Nav component</h3>
+// <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Get Weather</IndexLink>
+// <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+// <IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+// </div>
