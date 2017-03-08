6123115f8739f4065055a1a2053d5b5f3dc065d9
mwakhare <mwakhare@torinit.com>
2017-03-08 12:23:44 +0530

    Fatching data message added.

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,30 +6,46 @@ const openweathermap = require('../api/openweathermap');
 var Weather = React.createClass ({
   getInitialState: function(){
     return ({
-      location: 'Navi Mumbai',
-      temp: 35
+      isLoading: false
     });
   },
   handleSearch: function(location){
     var that = this;
 
+    that.setState ({
+      isLoading: true
+    });
+
     openweathermap.getTemp (location).then(function (temp) {
       that.setState ({
         location: location,
-        temp: temp
+        temp: temp,
+        isLoading: false
       });
     }, function (errorMessage) {
-      alert (errorMessage);
+      that.setState ({
+        isLoading: false
+      });
     })
   },
   render: function (){
-    var {temp, location} = this.state;
+    var {isLoading, temp, location} = this.state;
+
+      function renderMessage () {
+      if (isLoading)
+      {
+        return <h2>Fatching the data...</h2>;
+      }else {
+        return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
+
     return (
       <div>
         <h4>Weather component</h4>
 
         <WeatherForm onSearch={this.handleSearch}/>
-        <WeatherMessage temp={temp} location={location}/>
+        {renderMessage()}
       </div>
 
     );
