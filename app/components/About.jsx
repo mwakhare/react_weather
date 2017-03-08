c493633bf5e3ecc9beca52090c65a158edbf5451
mwakhare <mwakhare@torinit.com>
2017-03-08 17:09:46 +0530

    about page foundation style added.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,26 @@ var React = require ('react');
 var About = React.createClass ({
   render: function (){
     return (
-      <h4>About component</h4>
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>
+          This app is based on React.
+          This is build for practice purpose.
+
+          I have used following tools-
+        </p>
+
+        <ul>
+          <li>
+            <a herf="https://facebook.github.io/react">React  - This was
+              the JavaScript framework used.</a>
+          </li>
+          <li>
+            <a herf="http://openweathermap.com">Open Weather Map - This was
+              used to get weather data by city name.</a>
+          </li>
+        </ul>
+      </div>
     );
   }
 });
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require ('react');
+var {Link} = require ('react-router');
 
 var Examples = React.createClass ({
   render: function (){
     return (
-      <h4>Examples component</h4>
+      <div>
+      <h1>Examples</h1>
+      <p>Here are some example locatioins:</p>
+      <ol>
+        <li>
+          <Link to="?location=Nashik">Nashik</Link>
+        </li>
+        <li>
+          <Link to="?location=Thane">Thane</Link>
+        </li>
+      </ol>
+      </div>
     );
   }
 });
